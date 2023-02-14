c0088b8216ff85c86e92113770d2d592b46e8cd8
mic-v <michaelvictorino@gmail.com>
2023-02-14 12:54:26 -0500

    Added mobile responsiveness

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import './styles/main.css';
 
 import logo from './assets/logo.jpg';
-import landingImg from './assets/heroimage.JPG';
-import flowImg from './assets/FlowG.PNG';
-import skustaImg from './assets/SkustaClee.PNG';
-import jroaImg from './assets/JRoa.PNG';
+import landingImg from './assets/heroimage.webp';
+import flowImg from './assets/FlowG.webp';
+import skustaImg from './assets/SkustaClee.webp';
+import jroaImg from './assets/JRoa.webp';
 import dianaImg from './assets/Diana.PNG';
 import alvaroImg from './assets/DJAlvaro.jpg';
-import bambooImg from './assets/bambooposter.jpg';
+import bambooImg from './assets/bambooposter.webp';
+import eraserImg from './assets/eraserheadsposter.webp';
 
 
 function loadImages() {
@@ -19,25 +20,31 @@ function loadImages() {
     //IMAGES
 
     const landingImage = document.getElementById('landingImage');
-    landingImage.src = landingImg;
+    if(landingImage) landingImage.src = landingImg;
 
     const skustaImage = document.getElementById('skustaImage');
-    skustaImage.src = skustaImg;
+    if(skustaImage) skustaImage.src = skustaImg;
 
     const jroaImage = document.getElementById('jroaImage');
-    jroaImage.src = jroaImg;
+    if(jroaImage) jroaImage.src = jroaImg;
 
     const flowImage = document.getElementById('flowImage');
-    flowImage.src = flowImg;
+    if(flowImage) flowImage.src = flowImg;
 
+    const bambooImage = document.getElementById('bambooImage');
+    if(bambooImage) bambooImage.src = bambooImg;
+
+    const eraserImage = document.getElementById('eraserImage');
+    if(eraserImage) eraserImage.src = eraserImg;
 }
 
 window.onscroll = function (event) {
     let scroll = window.pageYOffset;
     const navBar = document.getElementById('navBar');
-    if(scroll < 25) {
+    if(scroll < 25 && window.innerWidth > 640) {
         if(!navBar.classList.contains('bg-transparent')) {
             navBar.classList.add('bg-transparent');
+            navBar.classList.add('md:bg-transparent');
             navBar.classList.remove('bg-slate-800');
             navBar.classList.remove('border-slate-800');
             navBar.classList.remove('border-b');
@@ -48,8 +55,20 @@ window.onscroll = function (event) {
             navBar.classList.add('border-slate-800');
             navBar.classList.add('border-b');
             navBar.classList.remove('bg-transparent');
+            navBar.classList.remove('md:bg-transparent');
         }
     }
 }
+
+function classToggle() {
+    const navs = document.getElementById('navlist');
+    const socials = document.getElementById('sociallist');
+    navs.classList.toggle('flex');
+    navs.classList.toggle('hidden');
+    socials.classList.toggle('flex');
+    socials.classList.toggle('hidden');
+}
+
+document.querySelector('#navlist-toggle').addEventListener('click', classToggle);
 
 loadImages();
--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
         type: 'asset/resource',
       },
     ]
